32e0abedfd948934f6b6bef8cd5696381567edda
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-09-18 21:18:09 -0400

    commit tree 7920b2fcf145dc8fe4ae1411ad8c0e0e171d1d01

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function () {
+    getBooks()
+    showBookDetails()
+});
+
+function getBooks() {
+    fetch('http://localhost:3000/books')
+    .then(res => res.json())
+    .then(books => books.map(book => {
+        const booksContainer = document.getElementById('list-panel')
+        const li = document.createElement('li')
+        li.innerText = book.title
+        booksContainer.append(li)
+
+        li.addEventListener('click', (e) => showBookDetails(e, book))
+    }))
+}
+
+function showBookDetails(e, book) {
+    e.preventDefault()
+    const showPanel = document.getElementById('show-panel')
+    showPanel.innerHTML = ''
+    const image = document.createElement('img')
+    image.src = book.img_url
+
+    const titleH1 = document.createElement('h1')
+    titleH1.innerText = book.title
+
+    const description = document.createElement('p')
+    description.innerText = book.description
+
+    const userUL = document.createElement('ul')
+
+    book.users.forEach(user => {
+        const userLI = document.createElement('li')
+        userLI.innerText = user.username
+        userUL.append(userLI)
+    })
+
+    showPanel.append(titleH1, image, description, userUL)
+}
+
+// likes fxn and event listener - patch request within the function
